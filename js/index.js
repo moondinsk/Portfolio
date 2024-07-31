603b26f669618ef20b18eef40cf49a65d390674e
moondinsk <moondinsk@gmail.com>
2024-07-31 23:46:20 +0900

    layout

--- js/index.js
+++ js/index.js
@@ -1,23 +1,59 @@
-// Main Setting
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-    });
+let cateIdx = 0;
+let mainIdx = 0;
 
-    // 슬라이드 변경 
-    swiper.on('slideChange', function () {
-    const realIndex = swiper.realIndex;
+/****** Main Setting ******/
+const main_sw = new Swiper('#sw-main', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// 슬라이드 변경 
+main_sw.on('slideChange', function () {
+  mainIdx = main_sw.realIndex;
+});
+
+
+
+/****** Click Events ******/
+// 메인 - view-more 
+$('.view-more').on("click",function (){
+  toggleMain();
+  toggleWorks(mainIdx);
+});
+
+// 섹션 - close 
+$('.__works-close').on("click", function (){
+  toggleMain();
+  toggleWorks(mainIdx);
+});
+
+// 메인 열고닫기
+function toggleMain(){
+  $("#main").toggleClass("show");
+  $("#header_right").toggleClass("active")
+}
+
+// 섹션 idx 찾아 열고닫기
+function toggleWorks(mainIdx){
+  $("#works").toggleClass("show");
+  $("#works > section").eq(mainIdx).toggleClass("show");
+}
+
+
+
+/****** About Setting ******/
+/****** Click Events ******/
+$(".__ui-page").on("click", function (){
+  $(this).parent(".ui-page__item").addClass("active").siblings().removeClass("active");
+  cateIdx = $(this).parent(".ui-page__item").index();
+  $("#page > *").eq(cateIdx).addClass("show").siblings().removeClass("show");
 });
